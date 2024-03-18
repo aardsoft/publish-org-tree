7fdbc4629149e8d73ee5adfbdd7782fc7e24222f
Bernd Wachter <bernd-github@wachter.fi>
2024-03-18 14:53:39 +0200

    Run with xvfb, if available
    Newer emacs seem to break image stuff in batch mode

--- publish-org-tree.sh
+++ publish-org-tree.sh
@@ -3,4 +3,8 @@
 
 me=`realpath $0`
 my_dir=`dirname $me`
-emacs --batch -Q -L . -eval "(progn (add-to-list 'load-path \"$my_dir\")(load-file \"$my_dir/publish-org-tree.el\")(publish-project))"
+if [ `which xvfb-run` ]; then
+    xvfb-run emacs -Q -L . -eval "(progn (add-to-list 'load-path \"$my_dir\")(load-file \"$my_dir/publish-org-tree.el\")(publish-project)(save-buffers-kill-terminal))"
+else
+    emacs --batch -Q -L . -eval "(progn (add-to-list 'load-path \"$my_dir\")(load-file \"$my_dir/publish-org-tree.el\")(publish-project))"
+fi
